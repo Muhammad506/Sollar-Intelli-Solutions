7c310fab2eec7f12071e90f6ac34974a8f022ffe
Muhammad Umer Saleem <66533954+Muhammad506@users.noreply.github.com>
2024-11-11 18:25:21 +0500

    Update Index.js

--- BackEnd/Index.js
+++ BackEnd/Index.js
@@ -9,7 +9,13 @@ const PORT = process.env.PORT || 5000;
 // Connect to the database
 connectDB();
 
-app.use(cors());
+app.use(cors(
+      {
+    origin: ["https://solar-intelli-solutions.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  }
+));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
